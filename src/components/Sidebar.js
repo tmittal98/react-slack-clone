a507204947b7416046d153d23eab907a11231ebd
Tushar <tusharmittal429@gmail.com>
2020-10-10 03:46:47 +0530

    Add Sidebar HTML

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Sidebar extends Component {
   render() {
-    return <div>Sidebar</div>;
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div>
+            <img />
+          </div>
+          <div>Tushar</div>
+        </div>
+        <div className="user-channels"></div>
+        <div className="user-profile"></div>
+      </div>
+    );
   }
 }
 
