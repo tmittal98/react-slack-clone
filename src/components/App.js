8254d90916164cf1b976cc49446b546d39421b87
Tushar <tusharmittal429@gmail.com>
2020-10-10 00:56:22 +0530

    add SignIn component and .prettierrc

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { SignIn } from "./";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      Hello World
+      <SignIn />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App, SignIn } from "./components"; //here we do not need to write index file it will automatically understand
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
